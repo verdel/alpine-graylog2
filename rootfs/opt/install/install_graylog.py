f4efa34b5990229b5ff2bb108b2e8bb4dc26ec27
Vadim Aleksandrov <valeksandrov@me.com>
2020-01-03 15:58:53 +0300

    Fix install script. Change regex for capturing graylog download link

--- rootfs/opt/install/install_graylog.py
+++ rootfs/opt/install/install_graylog.py
@@ -6,7 +6,7 @@ import re
 
 if __name__ == '__main__':
     r = requests.get('https://www.graylog.org/downloads')
-    match = re.match('.*<a href="(.*graylog-(.*).tgz)"', r.text, re.DOTALL | re.M)
+    match = re.match('.*<a href="(.*graylog-(\d\.\d\.\d).tgz)"', r.text, re.DOTALL | re.M)
     if match:
         r = requests.get(match.group(1))
         with open('/opt/graylog-{}.tgz'.format(match.group(2)), 'wb') as f:
